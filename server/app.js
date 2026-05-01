5d831d28942c937144183a4a1d3a1eabb468e1aa
Abderrahmanemenacer <89222974+Abderrahmanemenacer@users.noreply.github.com>
2026-05-01 22:09:12 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // ✅ CORS with credentials for cookie-based auth
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174","https://international-scientific-conference-portal-hqayx1ujk.vercel.app"],
+  origin: ["http://localhost:5173", "http://localhost:5174","*"],
   credentials: true,
 }));
 
